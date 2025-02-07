6fbe7f1506f1b636f76a87c3eea641ffadc51c64
jskno <lodmnos@gmail.com>
2025-02-07 15:36:55 +0100

    QuickUnion Improvements

--- module_2/src/main/java/com/jskno/d_weighted_quick_union/WeightedQuickUnionUF.java
+++ module_2/src/main/java/com/jskno/d_weighted_quick_union/WeightedQuickUnionUF.java
@@ -1,30 +1,45 @@
-package com.jskno.c_quick_union;
+package com.jskno.d_weighted_quick_union;
 
-public class QuickUnionUF {
+public class WeightedQuickUnionUF {
 
     private final int[] id;
+    private final int[] sz;
 
     // Set id of each object to itself
-    // (N array accesses)
-    public QuickUnionUF(int n) {
+    // (2N array accesses)
+    public WeightedQuickUnionUF(int n) {
         id =  new int[n];
+        sz =  new int[n];
         for (int i = 0; i < n; ++i) {
             id[i] = i;
+            sz[i] = 1;
         }
     }
 
     // Check if p and q have same root
     // depth of p and q array accesses)
+    // Depth of any node x is at most logN
+    // So 2logN array accesses at most
     public boolean connected(int p, int q) {
         return root(p) == root(q);
     }
 
     // Change root of p to point to root of q
     // (depth of p and q array accesses)
+    // 2logN
     public void union(int p, int q) {
         int i = root(p);
         int j = root(q);
-        id[i] = j;
+        if (i == j) {
+            return;
+        }
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
     }
 
     // Chase parent pointers until reach root
